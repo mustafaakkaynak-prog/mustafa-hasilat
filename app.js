3e76e90e27423054bb5cbab3674fa2711cf02f17
mustafaakkaynak-prog <mustafaakkaynak@gmail.com>
2025-11-03 17:44:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,3 +29,43 @@ function saveToLocal(type, data) {
   localStorage.setItem("stok", JSON.stringify(existing));
   alert(type + " kaydedildi ✅");
 }
+function downloadExcel() {
+    const stok = JSON.parse(localStorage.getItem("stok") || "{}");
+
+    let workbook = {
+        "OKUL": [],
+        "YURT KIZ": [],
+        "YURT ERKEK": []
+    };
+
+    const products = Object.keys(JSON.parse(localStorage.getItem("product_map") || "{}"));
+
+    products.forEach(product => {
+        workbook["OKUL"].push({
+            "Ürün": product,
+            "Gelen": stok.okul_gelen?.[product] || 0,
+            "Çıkan": (stok.kiz_cikis?.[product] || 0) + (stok.erkek_cikis?.[product] || 0),
+            "Kalan": stok.okul_kalan?.[product] || 0
+        });
+
+        workbook["YURT KIZ"].push({
+            "Ürün": product,
+            "Gelen": stok.kiz_cikis?.[product] || 0,
+            "Kalan": stok.kiz_kalan?.[product] || 0
+        });
+
+        workbook["YURT ERKEK"].push({
+            "Ürün": product,
+            "Gelen": stok.erkek_cikis?.[product] || 0,
+            "Kalan": stok.erkek_kalan?.[product] || 0
+        });
+    });
+
+    let file = "HASILAT_" + new Date().toISOString().slice(0,10) + ".xlsx";
+
+    let blob = new Blob([JSON.stringify(workbook, null, 2)], { type: "application/json" });
+    let a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = file;
+    a.click();
+}
